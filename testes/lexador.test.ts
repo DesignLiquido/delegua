87e12fee2af0fda1b967c1595118059f4bd84543
Leonel Sanches da Silva <leonel@designliquido.com.br>
2022-03-30 18:09:53 -0700

    Exemplo de primeiro teste.

--- testes/lexador.test.ts
+++ testes/lexador.test.ts
@@ -5,6 +5,16 @@ describe('lexador', () => {
     it('mapear()', () => {
         const delegua = new Delegua('delegua');
         const resultado = delegua.lexador.mapear("esceva('Olá mundo')");
+
         expect(resultado).toBeTruthy();
+        expect(resultado).toHaveLength(4);
+        expect(resultado).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ tipo: 'IDENTIFICADOR' }),
+                expect.objectContaining({ tipo: 'PARENTESE_ESQUERDO' }),
+                expect.objectContaining({ tipo: 'TEXTO' }),
+                expect.objectContaining({ tipo: 'PARENTESE_DIREITO' }),
+            ])
+        );
     });
 });
