0dd0707eb4f4d7412b849881b551df34ac4a1939
Samuel Renan Gonçalves Vaz <samuel.rvg@gmail.com>
2022-07-01 01:21:35 -0300

    Aumentando cobertura testes (#114)
    * Adicionando eguap nas keywords
    * Atualizando versão
    * Testando o núcleo da linguagem

---
+++ testes/delegua.test.ts
@@ -0,0 +1,18 @@
+import { Delegua } from "../fontes/delegua";
+
+describe('Delégua', () => {
+    let delegua: Delegua;
+
+    beforeEach(() => {
+        delegua = new Delegua('delegua');
+    });
+
+    describe('Sucesso', () => {        
+        it('Obter versão Delégua', () => {
+            const versaoDelegua = delegua.versao();
+
+            expect(versaoDelegua).toBeTruthy();
+        });
+    });
+
+});
---
+++ testes/egua-classico/delegua.test.ts
@@ -0,0 +1,11 @@
+import { Delegua } from "../../fontes/delegua";
+
+describe('Delégua - (Égua Clássico)', () => {
+
+    describe('Falha', () => {        
+        it('Depurador', () => {
+            expect(() => new Delegua('egua', false, true)).toThrow(new Error('Dialeto egua não suporta depuração.'));
+        });
+    });
+
+});
