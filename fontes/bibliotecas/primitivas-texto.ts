b83a58df1e0900cbbc80e14d351a10f53dd4a8bc
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-09-30 12:13:52 -0300

    Metodos de texto (#134)
    * Adiciona método maiusculo e sei teste em delegua
    * Implementação de interpolação de texto para literais
    * Testes de template literals + JSDocs
    * Implementa métodos de texto
    
    Co-authored-by: Leonel Sanches da Silva <leonel@designliquido.com.br>

--- fontes/bibliotecas/primitivas-texto.ts
+++ fontes/bibliotecas/primitivas-texto.ts
@@ -1,4 +1,8 @@
 export default {
     'minusculo': (texto: string) => texto.toLowerCase(),
-    'maiusculo': (texto: string) => texto.toUpperCase()
+    'maiusculo': (texto: string) => texto.toUpperCase(),
+    'inclui': (texto: string, elemento: any) => texto.includes(elemento),
+    "substituir": (texto: string, elemento: string, substituto: string) => texto.replace(elemento, substituto),
+    "subtexto": (texto: string, inicio: number, fim: number) => texto.slice(inicio, fim),
+    "dividir": (texto: string, divisor: any, limite: number) => ([...texto.split(divisor, limite)]),
 }
