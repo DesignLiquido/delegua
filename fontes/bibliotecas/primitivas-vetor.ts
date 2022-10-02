3762d75ac08224c53bce11da01904301f592f632
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-10-02 19:03:26 -0300

    Adiciona métodos de lista que não requerem callbacks e testes (#137)

--- fontes/bibliotecas/primitivas-vetor.ts
+++ fontes/bibliotecas/primitivas-vetor.ts
@@ -1,3 +1,10 @@
 export default {
-    'inclui': (vetor: Array<any>, elemento: any) => vetor.includes(elemento)
+    'inclui': (vetor: Array<any>, elemento: any) => vetor.includes(elemento),
+    'juntar': (vetor: Array<any>, separador: any) => vetor.join(separador),
+    'removerUltimo': (vetor: Array<any>) => vetor.pop(),
+    'removerPrimeiro': (vetor: Array<any>) => vetor.shift(),
+    'empilhar': (vetor: Array<any>, elemento: any) => vetor.push(elemento),
+    'inverter': (vetor: Array<any>) => vetor.reverse(),
+    'fatia': (vetor: Array<any>, inicio: number, fim: number) => vetor.slice(inicio, fim),
+    'ordenar': (vetor: Array<any>) => vetor.sort(),
 }
