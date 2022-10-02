e953d239fb7998adbaf7f3268ce14c75e459a87b
Leonel Sanches da Silva <leonel@designliquido.com.br>
2022-10-02 00:00:53 -0700

    Alguma expansão dos tipos de símbolos e palavras reservadas.

--- fontes/lexador/dialetos/palavras-reservadas/visualg.ts
+++ fontes/lexador/dialetos/palavras-reservadas/visualg.ts
@@ -2,7 +2,11 @@ import tiposDeSimbolos from "../../../tipos-de-simbolos/visualg";
 
 export default {
     algoritmo: tiposDeSimbolos.ALGORITMO,
+    ate: tiposDeSimbolos.ATE,
+    de: tiposDeSimbolos.DE,
+    faca: tiposDeSimbolos.FACA,
     fimalgoritmo: tiposDeSimbolos.FIMALGORITMO,
+    fimpara: tiposDeSimbolos.FIMPARA,
     inicio: tiposDeSimbolos.INICIO,
     var: tiposDeSimbolos.VAR
 }
