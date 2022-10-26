332251f53cbe28746162a89bb386020200e2026d
Vitor Brandão <82619724+VitBrandao@users.noreply.github.com>
2022-10-26 17:40:56 -0300

    implementa sinônimos de métodos de acordo com discussion (#143)

--- fontes/bibliotecas/primitivas-vetor.ts
+++ fontes/bibliotecas/primitivas-vetor.ts
@@ -4,8 +4,9 @@ export default {
     'removerUltimo': (vetor: Array<any>) => { vetor.pop(); return vetor; },
     'removerPrimeiro': (vetor: Array<any>) => { vetor.shift(); return vetor; },
     'empilhar': (vetor: Array<any>, elemento: any) => { vetor.push(elemento); return vetor; },
+    'adicionar': (vetor: Array<any>, elemento: any) => { vetor.push(elemento); return vetor; },
     'inverter': (vetor: Array<any>) => vetor.reverse(),
-    'fatia': (vetor: Array<any>, inicio: number, fim: number) => vetor.slice(inicio, fim),
+    'fatiar': (vetor: Array<any>, inicio: number, fim: number) => vetor.slice(inicio, fim),
     'ordenar': (vetor: Array<any>) => vetor.sort(),
     'somar': (vetor: Array<number>) => vetor.reduce((a, b) => a + b),
     'remover': (vetor: Array<any>, elemento: any) => {
