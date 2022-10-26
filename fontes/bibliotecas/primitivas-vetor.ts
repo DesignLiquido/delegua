d9643d7522e8c6375525dc4e440f0f61f3d3187e
VitBrandao <vitorsbsilva@gmail.com>
2022-10-26 12:23:49 -0300

    Implementando e corrigindo Primitivas de Vetor e de Texto

--- fontes/bibliotecas/primitivas-vetor.ts
+++ fontes/bibliotecas/primitivas-vetor.ts
@@ -1,10 +1,16 @@
 export default {
     'inclui': (vetor: Array<any>, elemento: any) => vetor.includes(elemento),
-    'juntar': (vetor: Array<any>, separador: any) => vetor.join(separador),
-    'removerUltimo': (vetor: Array<any>) => vetor.pop(),
-    'removerPrimeiro': (vetor: Array<any>) => vetor.shift(),
-    'empilhar': (vetor: Array<any>, elemento: any) => vetor.push(elemento),
+    'juntar': (vetor: Array<any>, separador: string) => vetor.join(separador),
+    'removerUltimo': (vetor: Array<any>) => { vetor.pop(); return vetor; },
+    'removerPrimeiro': (vetor: Array<any>) => { vetor.shift(); return vetor; },
+    'empilhar': (vetor: Array<any>, elemento: any) => { vetor.push(elemento); return vetor; },
     'inverter': (vetor: Array<any>) => vetor.reverse(),
     'fatia': (vetor: Array<any>, inicio: number, fim: number) => vetor.slice(inicio, fim),
     'ordenar': (vetor: Array<any>) => vetor.sort(),
+    'somar': (vetor: Array<number>) => vetor.reduce((a, b) => a + b),
+    'remover': (vetor: Array<any>, elemento: any) => {
+        const index = vetor.indexOf(elemento);
+        if (index !== -1) vetor.splice(index, 1);
+        return vetor;
+    },
 }
