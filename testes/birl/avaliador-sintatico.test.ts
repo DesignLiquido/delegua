fe6ae37a3c8cdc53e65ef57d28190d6d9d100416
ItaloCobains <monokatchaleger@gmail.com>
2022-11-07 20:51:51 -0300

    Criando testes

---
+++ testes/birl/avaliador-sintatico.test.ts
@@ -0,0 +1,23 @@
+import { Delegua } from '../../fontes/delegua';
+
+describe('Avaliador sintático Birl', () => {
+    describe('analisar()', () => {
+        let delegua: Delegua;
+
+        beforeEach(() => {
+            delegua = new Delegua('birl');
+        });
+
+        it('Sucesso - Hello, World! Porra!', () => {
+            const retornoLexador = delegua.lexador.mapear(
+                ['HORA DO SHOW', 'CE QUER VER ESSA PORRA? ("Hello, World! Porra!\n");', 'BORA CUMPADE 0;', 'BIRL'],
+                -1
+            );
+
+            const retornoAvaliadorSintatico = delegua.avaliadorSintatico.analisar(retornoLexador);
+
+            expect(retornoAvaliadorSintatico).toBeTruthy();
+            expect(retornoAvaliadorSintatico.declaracoes).toHaveLength(4);
+        });
+    });
+});
