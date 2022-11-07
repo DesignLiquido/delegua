e25bda715646f2d6bdd57cbf16b0db9e30a6a124
ItaloCobains <monokatchaleger@gmail.com>
2022-11-07 10:00:21 -0300

    tentando implementar o primario

--- fontes/avaliador-sintatico/dialetos/avaliador-sintatico-birl.ts
+++ fontes/avaliador-sintatico/dialetos/avaliador-sintatico-birl.ts
@@ -1,11 +1,35 @@
-import { Construto, Funcao } from '../../construtos';
+import { Construto, Funcao, Literal, Variavel } from '../../construtos';
 import { Escreva, Declaracao, Enquanto, Para, Escolha, Fazer } from '../../declaracoes';
 import { RetornoLexador, RetornoAvaliadorSintatico } from '../../interfaces/retornos';
 import { AvaliadorSintaticoBase } from '../avaliador-sintatico-base';
+import tiposDeSimbolos from '../../tipos-de-simbolos/birl';
+import { SimboloInterface } from '../../interfaces';
 
 export class AvaliadorSintaticoBirl extends AvaliadorSintaticoBase {
     primario(): Construto {
-        throw new Error('Method not implemented.');
+        // Creio q isso e para fazer a criação de variaveis
+        // mais não ha nenhum prefixo para criar variavel
+        // nessa linguagem
+        // acho q terei que pegar essa verificação com base no '='
+        if (this.verificarSeSimboloAtualEIgualA(tiposDeSimbolos.IGUAL)) {
+            return new Variavel(-1, this.simbolos[this.atual - 1]);
+        }
+
+        // não tenho certeza mais isso deve ser pra tipar a variavel criada a cima.
+        if (
+            this.verificarSeSimboloAtualEIgualA(
+                tiposDeSimbolos.FR,
+                tiposDeSimbolos.M1,
+                tiposDeSimbolos.M2,
+                tiposDeSimbolos.M3,
+                tiposDeSimbolos.T,
+                tiposDeSimbolos.TD,
+                tiposDeSimbolos.BF
+            )
+        ) {
+            const simboloAnterior: SimboloInterface = this.simbolos[this.atual - 1];
+            return new Literal(-1, Number(simboloAnterior.linha), simboloAnterior.literal);
+        }
     }
     chamar(): Construto {
         throw new Error('Method not implemented.');
