7526cef00ab1667d020dd5971b5410838c318c1c
Leonel Sanches da Silva <leonel@designliquido.com.br>
2022-11-11 11:25:44 -0800

    Subida de versão para REPL.

--- fontes/delegua.ts
+++ fontes/delegua.ts
@@ -168,10 +168,10 @@ export class Delegua implements DeleguaInterface {
             const manifesto = caminho.resolve('package.json');
             return (
                 JSON.parse(fs.readFileSync(manifesto, { encoding: 'utf8' }))
-                    .version || '0.8'
+                    .version || '0.9'
             );
         } catch (error: any) {
-            return '0.8 (desenvolvimento)';
+            return '0.9 (desenvolvimento)';
         }
     }
 
