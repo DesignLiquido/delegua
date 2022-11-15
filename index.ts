cdfa3917c922b341ce8f4388f099048d6621036e
Leonel Sanches da Silva <leonel@designliquido.com.br>
2022-11-15 11:22:33 -0800

    Alterando ponto de entrada da execução da linguagem para outro arquivo que não seja o index.ts, usado por outras bibliotecas para importações.

--- index.ts
+++ index.ts
@@ -1,52 +1,6 @@
 export * from './fontes/delegua';
 export * from './fontes/lexador';
 export * from './fontes/avaliador-sintatico';
+export * from './fontes/importador';
 export * from './fontes/interpretador';
 
-import { Delegua } from './fontes/delegua';
-import { Command } from 'commander';
-
-const principal = () => {
-    const analisadorArgumentos = new Command();
-    let nomeArquivo: string;
-
-    analisadorArgumentos
-        .option(
-            '-d, --dialeto <dialeto>',
-            'Dialeto a ser usado. Padrão: delegua',
-            'delegua'
-        )
-        .option(
-            '-D, --depurador',
-            'Habilita o depurador, permitindo depuração em um ambiente como o VSCode. Sempre desabilitada em modo LAIR.',
-            false
-        )
-        .option(
-            '-p, --performance',
-            'Visualizar indicadores de performance. Desabilitado por padrão',
-            false
-        )
-        .argument('[arquivos...]', 'Nomes dos arquivos (opcional)')
-        .action((arquivos) => {
-            if (arquivos.length > 0) {
-                nomeArquivo = arquivos[0];
-            }
-        });
-
-    analisadorArgumentos.parse();
-    const opcoes = analisadorArgumentos.opts();
-
-    const delegua = new Delegua(
-        opcoes.dialeto,
-        opcoes.performance,
-        nomeArquivo ? opcoes.depurador : false
-    );
-
-    if (!nomeArquivo) {
-        delegua.iniciarLairDelegua();
-    } else {
-        delegua.carregarArquivo(nomeArquivo);
-    }
-};
-
-principal();
