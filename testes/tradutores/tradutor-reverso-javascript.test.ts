0799314f10972e57a6aeb83d6235ee695ac3d573
Samuel Vaz <samuel.rvg@gmail.com>
2023-01-07 17:04:54 -0300

    Testes unitários para tradutor reverso

--- testes/tradutores/tradutor-reverso-javascript.test.ts
+++ testes/tradutores/tradutor-reverso-javascript.test.ts
@@ -1,9 +1,4 @@
-import { Binario, Variavel, Literal } from '../../fontes/construtos';
-import { Se, Bloco, Escreva } from '../../fontes/declaracoes';
-import { Simbolo } from '../../fontes/lexador';
 import { TradutorReversoJavaScript } from '../../fontes/tradutores/tradutor-reverso-javascript';
-
-import tiposDeSimbolos from '../../fontes/tipos-de-simbolos/delegua';
 import { Delegua } from '../../fontes/delegua';
 
 describe('Tradutor Reverso Javascript -> Delégua', () => {
@@ -16,27 +11,55 @@ describe('Tradutor Reverso Javascript -> Delégua', () => {
             delegua = new Delegua('delegua');
         });
 
-        it('', () => {
-            // const codigo = 'const a = 42\nconst b = \'a\'\nconst c = 1 === 1\nconst d = 1 === \'1\'\nconst e = \'1\' === \'1\'\nconst f = 1 == 1';
-            // const codigo = 'const a = () => { console.log(\'Oi\') }';
-            // const codigo = 'function abc(parametro1){console.log(\'oi\')\nconsole.log(\'aaaa\')}'
-            // const codigo = 'console.log(\'Oi\')'
-            const codigo = `
-                class Rectangle {
-                    constructor(height, width, abc) {
-                        this.height = height;
-                        this.width = width;
-                    }
-
-                    teste(){
-                        console.log('oi')
-                    }
-                }
-            `
+        it('console.log -> escreva', () => {
+            const codigo = `console.log('Oi')`
 
             const resultado = tradutor.traduzir(codigo);
-            // expect(resultado).toBeTruthy();
-            // expect(resultado).toMatch(/console\.log\(i\)/i);
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/escreva\('Oi'\)/i);
         });
+
+        it('const/let/var -> var', () => {
+            const codigo = `const a = 1\nlet b = 2\nvar c = 3`
+
+            const resultado = tradutor.traduzir(codigo);
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/var a = 1/i);
+            expect(resultado).toMatch(/var b = 2/i);
+            expect(resultado).toMatch(/var c = 3/i);
+        });
+
+        it('\'function\' com parametro-> funcao com parametro', () => {
+            const codigo = `function teste(){console.log(\'Oi\')\nconsole.log(123)}`
+
+            const resultado = tradutor.traduzir(codigo);
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/funcao teste\(\)/i);
+            expect(resultado).toMatch(/escreva\('Oi'\)/i);
+            expect(resultado).toMatch(/escreva\(123\)/i);
+        });
+
+        // it('', () => {
+        //     // const codigo = 'const a = 42\nconst b = \'a\'\nconst c = 1 === 1\nconst d = 1 === \'1\'\nconst e = \'1\' === \'1\'\nconst f = 1 == 1';
+        //     // const codigo = 'const a = () => { console.log(\'Oi\') }';
+        //     // const codigo = 'function abc(parametro1){console.log(\'oi\')\nconsole.log(\'aaaa\')}'
+        //     // const codigo = 'console.log(\'Oi\')'
+        //     const codigo = `
+        //         class Rectangle {
+        //             constructor(height, width, abc) {
+        //                 this.height = height;
+        //                 this.width = width;
+        //             }
+
+        //             teste(){
+        //                 console.log('oi')
+        //             }
+        //         }
+        //     `
+
+        //     const resultado = tradutor.traduzir(codigo);
+        //     // expect(resultado).toBeTruthy();
+        //     // expect(resultado).toMatch(/console\.log\(i\)/i);
+        // });
     });
 });
