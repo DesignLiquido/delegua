8c3045c51393f4a49fce201fc1708ca198c1d59b
Leonel Sanches da Silva <leonel@designliquido.com.br>
2023-01-12 14:46:32 -0800

    Exportando formatadores.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export * from './fontes/delegua';
-export * from './fontes/lexador';
+
 export * from './fontes/avaliador-sintatico';
+export * from './fontes/formatadores';
 export * from './fontes/importador';
 export * from './fontes/interpretador';
-
+export * from './fontes/lexador';
