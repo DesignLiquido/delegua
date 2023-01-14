afc1ee69cab255c6170f3d7472f96257417bba33
Leonel Sanches da Silva <leonel@designliquido.com.br>
2023-01-14 09:39:17 -0800

    Repensando palavras reservadas de BIRL.

--- fontes/lexador/dialetos/palavras-reservadas/birl.ts
+++ fontes/lexador/dialetos/palavras-reservadas/birl.ts
@@ -1,16 +1,17 @@
 import tiposDeSimbolos from '../../../tipos-de-simbolos/birl';
 
 export default {
-    HORA_DO_SHOW: tiposDeSimbolos.HORA_DO_SHOW,
-    BIRL: tiposDeSimbolos.BIRL,
-    QUE_QUE_CE_QUER_MONSTRAO: tiposDeSimbolos.QUE_QUE_CE_QUER_MONSTRAO,
-    ELE_QUE_A_GENTE_QUER: tiposDeSimbolos.ELE_QUE_A_GENTE_QUER,
-    NAO_VAI_DAR_NAO: tiposDeSimbolos.NAO_VAI_DAR_NAO,
-    QUE_NAO_VAI_DAR_O_QUE: tiposDeSimbolos.QUE_NAO_VAI_DAR_O_QUE,
-    NEGATIVA_BAMBAM: tiposDeSimbolos.NEGATIVA_BAMBAM,
-    MAIS_QUERO_MAIS: tiposDeSimbolos.MAIS_QUERO_MAIS,
-    VAMO_MONSTRO: tiposDeSimbolos.VAMO_MONSTRO,
-    SAI_FILHO_DA_PUTA: tiposDeSimbolos.SAI_FILHO_DA_PUTA,
-    OH_O_HOME_AI_PO: tiposDeSimbolos.OH_O_HOME_AI_PO,
-    AJUDA_O_MALUCO_TA_DOENTE: tiposDeSimbolos.AJUDA_O_MALUCO_TA_DOENTE,
+    // HORA_DO_SHOW: tiposDeSimbolos.HORA_DO_SHOW,
+    // BIRL: tiposDeSimbolos.BIRL,
+    // QUE_QUE_CE_QUER_MONSTRAO: tiposDeSimbolos.QUE_QUE_CE_QUER_MONSTRAO,
+    // ELE_QUE_A_GENTE_QUER: tiposDeSimbolos.ELE_QUE_A_GENTE_QUER,
+    // NAO_VAI_DAR_NAO: tiposDeSimbolos.NAO_VAI_DAR_NAO,
+    // QUE_NAO_VAI_DAR_O_QUE: tiposDeSimbolos.QUE_NAO_VAI_DAR_O_QUE,
+    // NEGATIVA_BAMBAM: tiposDeSimbolos.NEGATIVA_BAMBAM,
+    // MAIS_QUERO_MAIS: tiposDeSimbolos.MAIS_QUERO_MAIS,
+    // VAMO_MONSTRO: tiposDeSimbolos.VAMO_MONSTRO,
+    // SAI_FILHO_DA_PUTA: tiposDeSimbolos.SAI_FILHO_DA_PUTA,
+    // OH_O_HOME_AI_PO: tiposDeSimbolos.OH_O_HOME_AI_PO,
+    // AJUDA_O_MALUCO_TA_DOENTE: tiposDeSimbolos.AJUDA_O_MALUCO_TA_DOENTE,
+    
 };
