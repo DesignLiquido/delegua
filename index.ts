d712006da3139dc28fb2e83fe0a6fbe7be1ae228
Leonel Sanches da Silva <leonel@designliquido.com.br>
2023-01-24 09:02:18 -0800

    Exportando mais objetos para melhorar suporte de Delégua na extensão do VSCode.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export * from './fontes/delegua';
 
 export * from './fontes/avaliador-sintatico';
+export { PontoParada, cyrb53 } from './fontes/depuracao';
 export * from './fontes/formatadores';
 export * from './fontes/importador';
 export * from './fontes/interpretador';
