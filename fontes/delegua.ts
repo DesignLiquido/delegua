e9091391fb6ba2a12d77fdfb1ee8f859a78de0a1
Samuel Vaz <samuel.rvg@gmail.com>
2023-02-02 12:21:43 -0300

    Ajustes

--- fontes/delegua.ts
+++ fontes/delegua.ts
@@ -290,7 +290,7 @@ export class Delegua implements DeleguaInterface {
      *                          Se verdadeiro, os arquivos de saída são escritos no mesmo diretório
      *                          do arquivo passado no primeiro parâmetro.
      */
-    traduzirArquivo(caminhoRelativoArquivo: string, gerarArquivoSaida: boolean): any {
+    traduzirArquivo(caminhoRelativoArquivo: string, gerarArquivoSaida: boolean, extensaoArquivoSaida: string): any {
         const caminhoAbsolutoPrimeiroArquivo = caminho.resolve(caminhoRelativoArquivo);
         const novoDiretorioBase = caminho.dirname(caminhoAbsolutoPrimeiroArquivo);
 
@@ -316,8 +316,7 @@ export class Delegua implements DeleguaInterface {
         if (gerarArquivoSaida) {
             ['.delegua', '.js'].map((extensao) => {
                 if (caminhoAbsolutoPrimeiroArquivo.includes(extensao)) {
-                    const extensaoSaida = extensao === '.delegua' ? '.js' : '.delegua';
-                    fs.writeFile(caminhoAbsolutoPrimeiroArquivo.replace(extensao, extensaoSaida), resultado, (erro) => {
+                    fs.writeFile(caminhoAbsolutoPrimeiroArquivo.replace(extensao, `.${extensaoArquivoSaida}`), resultado, (erro) => {
                         if (erro) throw erro;
                     });
                 }
