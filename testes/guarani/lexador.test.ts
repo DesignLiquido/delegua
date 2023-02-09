f3aff53507d3ac3797eec551c7a3772571a8a23f
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2023-02-09 08:12:59 -0800

    Demonstração de dialeto Guarani. (#270)

---
+++ testes/guarani/lexador.test.ts
@@ -0,0 +1,43 @@
+import { LexadorGuarani } from '../../fontes/lexador/dialetos/lexador-guarani';
+import tiposDeSimbolos from '../../fontes/tipos-de-simbolos/guarani';
+
+describe('Lexador', () => {
+    describe('mapear()', () => {
+        let lexador: LexadorGuarani;
+
+        beforeEach(() => {
+            lexador = new LexadorGuarani();
+        });
+
+        describe('Cenários de sucesso', () => {
+            it('Sucesso - Código vazio', () => {
+                const resultado = lexador.mapear([''], -1);
+
+                expect(resultado).toBeTruthy();
+                expect(resultado.simbolos).toHaveLength(0);
+            });
+
+            it('Sucesso - Ponto-e-vírgula, opcional', () => {
+                const resultado = lexador.mapear([';;;;;;;;;;;;;;;;;;;;;'], -1);
+
+                expect(resultado).toBeTruthy();
+                expect(resultado.simbolos).toHaveLength(0);
+            });
+
+            it('Sucesso - Olá guaranis', () => {
+                const resultado = lexador.mapear(["hai('Olá guaranis')"], -1);
+
+                expect(resultado).toBeTruthy();
+                expect(resultado.simbolos).toHaveLength(4);
+                expect(resultado.simbolos).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({ tipo: tiposDeSimbolos.HAI }),
+                        expect.objectContaining({ tipo: tiposDeSimbolos.PARENTESE_ESQUERDO }),
+                        expect.objectContaining({ tipo: tiposDeSimbolos.TEXTO }),
+                        expect.objectContaining({ tipo: tiposDeSimbolos.PARENTESE_DIREITO }),
+                    ])
+                );
+            });
+        });
+    });
+});
