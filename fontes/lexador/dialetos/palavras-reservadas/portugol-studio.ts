67152dbf0504c39db56fc633b5fa76d293bc05cf
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2023-02-11 08:00:27 -0800

    Portugol Studio: função `leia()`, primitiva de inteiro. (#275)
    * Função `leia()` para avaliador sintático do Portugol Studio.
    * Criação do Interpretador do Portugol Studio.

--- fontes/lexador/dialetos/palavras-reservadas/portugol-studio.ts
+++ fontes/lexador/dialetos/palavras-reservadas/portugol-studio.ts
@@ -3,5 +3,7 @@ import tiposDeSimbolos from '../../../tipos-de-simbolos/portugol-studio';
 export default {
     escreva: tiposDeSimbolos.ESCREVA,
     funcao: tiposDeSimbolos.FUNCAO,
+    inteiro: tiposDeSimbolos.INTEIRO,
+    leia: tiposDeSimbolos.LEIA,
     programa: tiposDeSimbolos.PROGRAMA,
 }
