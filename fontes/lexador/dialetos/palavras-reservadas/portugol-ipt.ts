eb098d0f0948b0eee1adfe3eff28cf15e9ae149b
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2023-02-17 18:43:17 -0800

    Portugol IPT - Lexador (#282)
    * Esboço de Lexador para o Portugol IPT.
    
    * Testes unitários para Lexador de Portugol IPT.
    
    * launch.json.

---
+++ fontes/lexador/dialetos/palavras-reservadas/portugol-ipt.ts
@@ -0,0 +1,7 @@
+import tiposDeSimbolos from '../../../tipos-de-simbolos/portugol-ipt';
+
+export default {
+    escrever: tiposDeSimbolos.ESCREVER,
+    fim: tiposDeSimbolos.FIM,
+    inicio: tiposDeSimbolos.INICIO
+};
