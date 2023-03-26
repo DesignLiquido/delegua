9957d3fc0b68587015ddc69f1c094d548d8dfd81
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2023-03-26 07:28:24 -0700

    Exportando interpretador com depuração para Portugol Studio.

--- fontes/interpretador/dialetos/index.ts
+++ fontes/interpretador/dialetos/index.ts
@@ -1,4 +1,5 @@
 export * from './egua-classico';
 export * from './portugol-studio/interpretador-portugol-studio';
+export * from './portugol-studio/interpretador-portugol-studio-com-depuracao';
 export * from './visualg/interpretador-visualg';
 export * from './visualg/interpretador-visualg-com-depuracao';
