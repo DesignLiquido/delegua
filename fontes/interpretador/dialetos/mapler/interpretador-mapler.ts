b9b41acac4f1266bfa6f4f0c753b2c0ab8de3ca8
Samuel Vaz <samuel.rvg@gmail.com>
2023-04-07 13:38:33 -0300

    Renomeando comentários

--- fontes/interpretador/dialetos/mapler/interpretador-mapler.ts
+++ fontes/interpretador/dialetos/mapler/interpretador-mapler.ts
@@ -32,7 +32,7 @@ export class InterpretadorMapler extends InterpretadorBase {
     }
 
     /**
-     * No VisuAlg, o bloco de condição executa se falso.
+     * No Mapler, o bloco de condição executa se falso.
      * Por isso a reimplementação aqui.
      * @param declaracao A declaração `Fazer`
      * @returns Só retorna em caso de erro na execução, e neste caso, o erro.
@@ -56,11 +56,11 @@ export class InterpretadorMapler extends InterpretadorBase {
 
     /**
      * Execução de uma escrita na saída padrão, sem quebras de linha.
-     * Implementada para alguns dialetos, como VisuAlg.
+     * Implementada para alguns dialetos, como Mapler.
      * 
      * Como `readline.question` sobrescreve o que foi escrito antes, aqui
      * definimos `this.mensagemPrompt` para uso com `leia`.
-     * No VisuAlg é muito comum usar `escreva()` seguido de `leia()` para
+     * No Mapler é muito comum usar `escreva()` seguido de `leia()` para
      * gerar um prompt na mesma linha.
      * @param declaracao A declaração.
      * @returns Sempre nulo, por convenção de visita.
