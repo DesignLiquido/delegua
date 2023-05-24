e07538493ee9a6d27b68a224154cc1800937bad3
Samuel Vaz <samuel.rvg@gmail.com>
2023-05-23 21:59:19 -0300

    Tradução delégua para python

--- fontes/tradutores/tradutor-python.ts
+++ fontes/tradutores/tradutor-python.ts
@@ -1,10 +1,91 @@
-import { Literal } from "../construtos";
-import { Declaracao, Escreva, Leia } from "../declaracoes";
-import { TradutorInterface } from "../interfaces";
+import { Agrupamento, Atribuir, Binario, Chamada, Literal, Logico, Variavel, Vetor } from "../construtos";
+import { Bloco, Declaracao, Escreva, Expressao, FuncaoDeclaracao, Leia, Para, ParaCada, Retorna, Var } from "../declaracoes";
+import { SimboloInterface, TradutorInterface } from "../interfaces";
+import tiposDeSimbolos from '../tipos-de-simbolos/delegua';
 
 export class TradutorPython implements TradutorInterface {
+    indentacao: number = 0;
+
+    traduzirSimboloOperador(operador: SimboloInterface): string {
+        switch (operador.tipo) {
+            case tiposDeSimbolos.ADICAO:
+                return '+';
+            case tiposDeSimbolos.BIT_AND:
+                return '&';
+            case tiposDeSimbolos.BIT_OR:
+                return '|';
+            case tiposDeSimbolos.BIT_XOR:
+                return '^';
+            case tiposDeSimbolos.BIT_NOT:
+                return '~';
+            case tiposDeSimbolos.DIFERENTE:
+                return '!=';
+            case tiposDeSimbolos.DIVISAO:
+                return '/';
+            case tiposDeSimbolos.E:
+                return 'and';
+            case tiposDeSimbolos.EXPONENCIACAO:
+                return '**';
+            case tiposDeSimbolos.IGUAL:
+                return '=';
+            case tiposDeSimbolos.IGUAL_IGUAL:
+                return '==';
+            case tiposDeSimbolos.MAIOR:
+                return '>';
+            case tiposDeSimbolos.MAIOR_IGUAL:
+                return '>=';
+            case tiposDeSimbolos.MENOR:
+                return '<';
+            case tiposDeSimbolos.MENOR_IGUAL:
+                return '<=';
+            case tiposDeSimbolos.MODULO:
+                return '%';
+            case tiposDeSimbolos.MULTIPLICACAO:
+                return '*';
+            case tiposDeSimbolos.OU:
+                return 'or';
+            case tiposDeSimbolos.SUBTRACAO:
+                return '-';
+        }
+    }
+
+    logicaComumBlocoEscopo(declaracoes: Declaracao[]): string {
+        let resultado = '';
+        this.indentacao += 4;
+
+        if (typeof declaracoes[Symbol.iterator] === 'function') {
+            for (const declaracaoOuConstruto of declaracoes) {
+                resultado += ' '.repeat(this.indentacao);
+                const nomeConstrutor = declaracaoOuConstruto.constructor.name;
+                if (this.dicionarioConstrutos.hasOwnProperty(nomeConstrutor)) {
+                    resultado += this.dicionarioConstrutos[nomeConstrutor](declaracaoOuConstruto);
+                } else {
+                    resultado += this.dicionarioDeclaracoes[nomeConstrutor](declaracaoOuConstruto);
+                }
+
+                resultado += '\n';
+            }
+        }
+
+        this.indentacao -= 4;
+        resultado += ' '.repeat(this.indentacao) + '\n';
+        return resultado;
+    }
+
+    traduzirConstrutoLogico(logico: Logico): string {
+        let direita = this.dicionarioConstrutos[logico.direita.constructor.name](logico.direita);
+        let operador = this.traduzirSimboloOperador(logico.operador);
+        let esquerda = this.dicionarioConstrutos[logico.esquerda.constructor.name](logico.esquerda);
+
+        return `${esquerda} ${operador} ${direita}`;
+    }
+    
     traduzirConstrutoLiteral(literal: Literal): string {
         if (typeof literal.valor === 'string') return `'${literal.valor}'`;
+        if (typeof literal.valor === 'boolean') {
+            return literal.valor ? 'True' : 'False';
+        }
+        if (!literal.valor) return 'None';
         return literal.valor;
     }
     
@@ -32,14 +113,163 @@ export class TradutorPython implements TradutorInterface {
 
         return resultado;
     }
+
+    traduzirConstrutoBinario(binario: Binario): string {
+        let resultado = '';
+        if (binario.esquerda.constructor.name === 'Agrupamento')
+            resultado += '(' + this.dicionarioConstrutos[binario.esquerda.constructor.name](binario.esquerda) + ')';
+        else resultado += this.dicionarioConstrutos[binario.esquerda.constructor.name](binario.esquerda);
+
+        let operador = this.traduzirSimboloOperador(binario.operador);
+        resultado += ` ${operador} `;
+
+        if (binario.direita.constructor.name === 'Agrupamento')
+            resultado += '(' + this.dicionarioConstrutos[binario.direita.constructor.name](binario.direita) + ')';
+        else resultado += this.dicionarioConstrutos[binario.direita.constructor.name](binario.direita);
+
+        return resultado;
+    }
+
+    traduzirDeclaracaoFuncao(declaracaoFuncao: FuncaoDeclaracao): string {
+        let resultado = 'def ';
+        resultado += declaracaoFuncao.simbolo.lexema + '(';
+
+        for (const parametro of declaracaoFuncao.funcao.parametros) {
+            resultado += parametro.nome.lexema + ', ';
+        }
+
+        if (declaracaoFuncao.funcao.parametros.length > 0) {
+            resultado = resultado.slice(0, -2);
+        }
+
+        resultado += '):';
+
+        resultado += this.logicaComumBlocoEscopo(declaracaoFuncao.funcao.corpo);
+        return resultado;
+    }
+
+    traduzirConstrutoChamada(chamada: Chamada): string {
+        let resultado = '';
+
+        // const retorno = `${this.dicionarioConstrutos[chamada.entidadeChamada.constructor.name](
+        //     chamada.entidadeChamada
+        // )}`;
+
+        // const instanciaClasse = this.declaracoesDeClasses.some((declaracao) => declaracao?.simbolo?.lexema === retorno);
+        // if (instanciaClasse) {
+        //     const classe = this.declaracoesDeClasses.find((declaracao) => declaracao?.simbolo?.lexema === retorno);
+        //     if (classe.simbolo.lexema === retorno) resultado += `new ${retorno}`;
+        // } else {
+        //     resultado += retorno;
+        // }
+        // resultado += '(';
+        // for (let parametro of chamada.argumentos) {
+        //     resultado += this.dicionarioConstrutos[parametro.constructor.name](parametro) + ', ';
+        // }
+        // if (chamada.argumentos.length > 0) {
+        //     resultado = resultado.slice(0, -2);
+        // }
+        // resultado += ')';
+        return resultado;
+    }
+
+    traduzirDeclaracaoRetorna(declaracaoRetorna: Retorna): string {
+        let resultado = 'return ';
+        const nomeConstrutor = declaracaoRetorna.valor.constructor.name;
+        return (resultado += this.dicionarioConstrutos[nomeConstrutor](declaracaoRetorna?.valor));
+    }
+
+    traduzirConstrutoVetor(vetor: Vetor): string {
+        if (!vetor.valores.length) {
+            return '[]';
+        }
+
+        let resultado = '[';
+
+        for (let valor of vetor.valores) {
+            resultado += `${this.dicionarioConstrutos[valor.constructor.name](valor)}, `;
+        }
+        if (vetor.valores.length > 0) {
+            resultado = resultado.slice(0, -2);
+        }
+
+        resultado += ']';
+
+        return resultado;
+    }
+
+    traduzirDeclaracaoVar(declaracaoVar: Var): string {
+        let resultado = '';
+        resultado += declaracaoVar.simbolo.lexema;
+        // if (!declaracaoVar?.inicializador) resultado += ';';
+        // else {
+        resultado += ' = ';
+        if (this.dicionarioConstrutos[declaracaoVar.inicializador.constructor.name]) {
+            resultado += this.dicionarioConstrutos[declaracaoVar.inicializador.constructor.name](
+                declaracaoVar.inicializador
+            );
+        } else {
+            resultado += this.dicionarioDeclaracoes[declaracaoVar.inicializador.constructor.name](
+                declaracaoVar.inicializador
+            );
+        }
+        // }
+        return resultado;
+    }
+
+    traduzirDeclaracaoParaCada(declaracaoParaCada: ParaCada): string {
+        let resultado = `for ${declaracaoParaCada.nomeVariavelIteracao} in `;
+        resultado +=
+            this.dicionarioConstrutos[declaracaoParaCada.vetor.constructor.name](declaracaoParaCada.vetor) + ":\n";
+
+        resultado += this.dicionarioDeclaracoes[declaracaoParaCada.corpo.constructor.name](declaracaoParaCada.corpo);
+        return resultado;
+    }
+
+    traduzirConstrutoAtribuir(atribuir: Atribuir): string {
+        let resultado = atribuir.simbolo.lexema;
+        resultado += ' = ' + this.dicionarioConstrutos[atribuir.valor.constructor.name](atribuir.valor);
+        return resultado;
+    }
+    
+    traduzirConstrutoVariavel(variavel: Variavel): string {
+        return variavel.simbolo.lexema;
+    }
+
+    traduzirDeclaracaoExpressao(declaracaoExpressao: Expressao): string {
+        return this.dicionarioConstrutos[declaracaoExpressao.expressao.constructor.name](declaracaoExpressao.expressao);
+    }
+
+    traduzirDeclaracaoBloco(declaracaoBloco: Bloco): string {
+        return this.logicaComumBlocoEscopo(declaracaoBloco.declaracoes);
+    }
+
+    traduzirConstrutoAgrupamento(agrupamento: Agrupamento): string {
+        return this.dicionarioConstrutos[agrupamento.constructor.name](agrupamento.expressao || agrupamento);
+    }
     
     dicionarioConstrutos = {
+        Agrupamento: this.traduzirConstrutoAgrupamento.bind(this),
+        Atribuir: this.traduzirConstrutoAtribuir.bind(this),
+        Binario: this.traduzirConstrutoBinario.bind(this),
+        Chamada: this.traduzirConstrutoChamada.bind(this),
         Literal: this.traduzirConstrutoLiteral.bind(this),
+        Logico: this.traduzirConstrutoLogico.bind(this),
+        Variavel: this.traduzirConstrutoVariavel.bind(this),
+        Vetor: this.traduzirConstrutoVetor.bind(this),
     }
 
     dicionarioDeclaracoes = {
+        Bloco: this.traduzirDeclaracaoBloco.bind(this),
+        Continua: () => 'continue',
         Escreva: this.traduzirDeclaracaoEscreva.bind(this),
+        Expressao: this.traduzirDeclaracaoExpressao.bind(this),
+        FuncaoDeclaracao: this.traduzirDeclaracaoFuncao.bind(this),
         Leia: this.traduzirDeclaracaoLeia.bind(this),
+        ParaCada: this.traduzirDeclaracaoParaCada.bind(this),
+        Retorna: this.traduzirDeclaracaoRetorna.bind(this),
+        Sustar: () => 'break',
+        Var: this.traduzirDeclaracaoVar.bind(this),
     }
 
     traduzir(declaracoes: Declaracao[]): string {
--- testes/tradutores/tradutor-python.test.ts
+++ testes/tradutores/tradutor-python.test.ts
@@ -27,5 +27,203 @@ describe('Tradutor Delégua -> Python', () => {
             expect(resultado).toBeTruthy();
             expect(resultado).toMatch(/print\('Olá mundo'\)/i);
         });
+
+        it('Agrupamento', () => {
+            const retornoLexador = lexador.mapear(
+                ['escreva((2 * 3) + (4 ^ 2))'], 
+                -1
+            );
+
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/print\(\(2 \* 3\) \+ \(4 \^ 2\)\)/i);
+        });
+
+        it('Atribuir', () => {
+            const retornoLexador = lexador.mapear(
+                [
+                    'var a = 1',
+                    'var b = \'1\'',
+                    'var c = verdadeiro',
+                    'var d = falso',
+                    'var e = nulo'
+                ], 
+                -1
+            );
+
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/a = 1/i);
+            expect(resultado).toMatch(/b = \'1\'/i);
+            expect(resultado).toMatch(/c = True/i);
+            expect(resultado).toMatch(/d = False/i);
+            expect(resultado).toMatch(/e = None/i);
+        });
+
+        it('Escreva verdadeiro e falso com operadores lógicos', () => {
+            const retornoLexador = lexador.mapear(
+                [
+                    'escreva(falso)',
+                    'escreva(verdadeiro)',
+                    'escreva(verdadeiro == verdadeiro)',
+                    'escreva(verdadeiro != verdadeiro)',
+                    'escreva(falso == falso)',
+                    'escreva(falso != falso)',
+                    'escreva(verdadeiro e falso)',
+                    'escreva(verdadeiro ou falso)',
+                ],
+                -1
+            );
+
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/print\(False\)/i);
+            expect(resultado).toMatch(/print\(True\)/i);
+            expect(resultado).toMatch(/print\(True == True\)/i);
+            expect(resultado).toMatch(/print\(True != True\)/i);
+            expect(resultado).toMatch(/print\(False == False\)/i);
+            expect(resultado).toMatch(/print\(False != False\)/i);
+            expect(resultado).toMatch(/print\(True and False\)/i);
+            expect(resultado).toMatch(/print\(True or False\)/i);
+        });
+
+        it('Escreva verdadeiro e falso com operadores lógicos', () => {
+            const retornoLexador = lexador.mapear(
+                [
+                    'funcao olaMundo () {',
+                    'escreva(\'Olá Mundo!!!\')',
+                    '}',
+                ],
+                -1
+            );
+
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/def olaMundo\(\):/i);
+            expect(resultado).toMatch(/print\(\'Olá Mundo!!!\'\)/i);
+        });
+
+        it('Escreva verdadeiro e falso com operadores lógicos', () => {
+            const retornoLexador = lexador.mapear(
+                [
+                    'funcao olaMundo (textoQualquer) {',
+                    'escreva(\'Olá Mundo!!!\', textoQualquer)',
+                    '}',
+                ],
+                -1
+            );
+
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/def olaMundo\(textoQualquer\):/i);
+            expect(resultado).toMatch(/print\(\'Olá Mundo!!!\', textoQualquer\)/i);
+        });
+
+        it('Para Cada \'em\' - vetor variável', async () => {
+            const retornoLexador = lexador.mapear([
+                "para cada elemento em [1, 2, 3] {",
+                "   escreva('Valor: ', elemento)",
+                "}",
+            ], -1);
+
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/for elemento in \[1, 2, 3\]:/i);
+            expect(resultado).toMatch(/print\(\'Valor: \', elemento\)/i);
+        });
+
+        it('função com retorno nulo -> function', () => {
+            const codigo = ['funcao minhaFuncao() { retorna nulo }'];
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/def minhaFuncao\(\):/i);
+            expect(resultado).toMatch(/return None/i);
+        });
+
+        it('função com retorno lógico de texto e número -> function', () => {
+            const codigo = ["funcao minhaFuncao() { retorna '1' == 1 }"];
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/def minhaFuncao\(\):/i);
+            expect(resultado).toMatch(/return '1' == 1/i);
+        });
+
+        it('função com retorno lógico de número -> function', () => {
+            const codigo = ['funcao minhaFuncao() { retorna 1 == 1 }'];
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/def minhaFuncao\(\):/i);
+            expect(resultado).toMatch(/return 1 == 1/i);
+        });
+
+        it('função com retorno lógico de texto -> function', () => {
+            const codigo = ["funcao minhaFuncao() { retorna '1' == '1' }"];
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/def minhaFuncao\(\):/i);
+            expect(resultado).toMatch(/return '1' == '1'/i);
+        });
+
+        it('função com retorno número -> function', () => {
+            const codigo = ['funcao minhaFuncao() { retorna 10 }'];
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/def minhaFuncao\(\):/i);
+            expect(resultado).toMatch(/return 10/i);
+        });
+
+        it('função com retorno texto -> function', () => {
+            const codigo = ["funcao minhaFuncao() { retorna 'Ola Mundo!!!' }"];
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/def minhaFuncao\(\):/i);
+            expect(resultado).toMatch(/return 'Ola Mundo!!!'/i);
+        });
+
+        it('função com retorno -> function', () => {
+            const codigo = ["funcao minhaFuncao() { retorna 'Ola Mundo!!!' }"];
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+            expect(resultado).toMatch(/def minhaFuncao\(\):/i);
+            expect(resultado).toMatch(/return 'Ola Mundo!!!'/i);
+        });
     });
 });
