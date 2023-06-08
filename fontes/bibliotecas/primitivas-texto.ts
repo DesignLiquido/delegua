414fb62a86131676c220c660629d026e0c7c10a0
Samuel Vaz <samuel.rvg@gmail.com>
2023-06-08 14:09:27 -0300

    Definindo retorno primitivas texto

--- fontes/bibliotecas/primitivas-texto.ts
+++ fontes/bibliotecas/primitivas-texto.ts
@@ -1,22 +1,22 @@
 import { VisitanteComumInterface } from "../interfaces";
 
 export default {
-    aparar: (interpretador: VisitanteComumInterface, texto: string): Promise<any> => Promise.resolve(texto.trim()),
-    apararFim: (interpretador: VisitanteComumInterface, texto: string): Promise<any> => Promise.resolve(texto.trimEnd()),
-    apararInicio: (interpretador: VisitanteComumInterface, texto: string): Promise<any> => Promise.resolve(texto.trimStart()),
-    concatenar: (interpretador: VisitanteComumInterface, ...texto: string[]): Promise<any> => Promise.resolve("".concat(...texto)),
+    aparar: (interpretador: VisitanteComumInterface, texto: string): Promise<string> => Promise.resolve(texto.trim()),
+    apararFim: (interpretador: VisitanteComumInterface, texto: string): Promise<string> => Promise.resolve(texto.trimEnd()),
+    apararInicio: (interpretador: VisitanteComumInterface, texto: string): Promise<string> => Promise.resolve(texto.trimStart()),
+    concatenar: (interpretador: VisitanteComumInterface, ...texto: string[]): Promise<string> => Promise.resolve("".concat(...texto)),
     dividir: (interpretador: VisitanteComumInterface, texto: string, divisor: any, limite?: number): Promise<string[]> => {
         if (limite) {
             return Promise.resolve(texto.split(divisor, limite))
         }
         return Promise.resolve(texto.split(divisor))
     },
-    fatiar: (interpretador: VisitanteComumInterface, texto: string, inicio: number, fim: number): Promise<any> => Promise.resolve(texto.slice(inicio, fim)),
-    inclui: (interpretador: VisitanteComumInterface, texto: string, elemento: any): Promise<any> => Promise.resolve(texto.includes(elemento)),
-    inverter: (interpretador: VisitanteComumInterface, texto: string): Promise<any> => Promise.resolve(texto.split('').reduce((texto, caracter) => texto = caracter + texto, '')),
-    maiusculo: (interpretador: VisitanteComumInterface, texto: string): Promise<any> => Promise.resolve(texto.toUpperCase()),
-    minusculo: (interpretador: VisitanteComumInterface, texto: string): Promise<any> => Promise.resolve(texto.toLowerCase()),
-    substituir: (interpretador: VisitanteComumInterface, texto: string, elemento: string, substituto: string): Promise<any> => Promise.resolve(texto.replace(elemento, substituto)),
-    subtexto: (interpretador: VisitanteComumInterface, texto: string, inicio: number, fim: number): Promise<any> => Promise.resolve(texto.slice(inicio, fim)),
-    tamanho: (interpretador: VisitanteComumInterface, texto: string): Promise<any> => Promise.resolve(texto.length),
+    fatiar: (interpretador: VisitanteComumInterface, texto: string, inicio: number, fim: number): Promise<string> => Promise.resolve(texto.slice(inicio, fim)),
+    inclui: (interpretador: VisitanteComumInterface, texto: string, elemento: any): Promise<boolean> => Promise.resolve(texto.includes(elemento)),
+    inverter: (interpretador: VisitanteComumInterface, texto: string): Promise<string> => Promise.resolve(texto.split('').reduce((texto, caracter) => texto = caracter + texto, '')),
+    maiusculo: (interpretador: VisitanteComumInterface, texto: string): Promise<string> => Promise.resolve(texto.toUpperCase()),
+    minusculo: (interpretador: VisitanteComumInterface, texto: string): Promise<string> => Promise.resolve(texto.toLowerCase()),
+    substituir: (interpretador: VisitanteComumInterface, texto: string, elemento: string, substituto: string): Promise<string> => Promise.resolve(texto.replace(elemento, substituto)),
+    subtexto: (interpretador: VisitanteComumInterface, texto: string, inicio: number, fim: number): Promise<string> => Promise.resolve(texto.slice(inicio, fim)),
+    tamanho: (interpretador: VisitanteComumInterface, texto: string): Promise<number> => Promise.resolve(texto.length),
 };
