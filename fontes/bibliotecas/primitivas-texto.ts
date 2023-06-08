27d32103bb44324c189f47ea811227215b7901b9
Samuel Vaz <samuel.rvg@gmail.com>
2023-06-08 13:29:08 -0300

    #441 método dividir ajustes

--- fontes/bibliotecas/primitivas-texto.ts
+++ fontes/bibliotecas/primitivas-texto.ts
@@ -5,7 +5,12 @@ export default {
     apararFim: (interpretador: VisitanteComumInterface, texto: string): Promise<any> => Promise.resolve(texto.trimEnd()),
     apararInicio: (interpretador: VisitanteComumInterface, texto: string): Promise<any> => Promise.resolve(texto.trimStart()),
     concatenar: (interpretador: VisitanteComumInterface, ...texto: string[]): Promise<any> => Promise.resolve("".concat(...texto)),
-    dividir: (interpretador: VisitanteComumInterface, texto: string, divisor: any, limite: number): Promise<any> => Promise.resolve(texto.split(divisor || ' ', limite)),
+    dividir: (interpretador: VisitanteComumInterface, texto: string, divisor: any, limite?: number): Promise<any> => {
+        if (limite) {
+            return Promise.resolve(texto.split(divisor, limite))
+        }
+        return Promise.resolve(texto.split(divisor))
+    },
     fatiar: (interpretador: VisitanteComumInterface, texto: string, inicio: number, fim: number): Promise<any> => Promise.resolve(texto.slice(inicio, fim)),
     inclui: (interpretador: VisitanteComumInterface, texto: string, elemento: any): Promise<any> => Promise.resolve(texto.includes(elemento)),
     inverter: (interpretador: VisitanteComumInterface, texto: string): Promise<any> => Promise.resolve(texto.split('').reduce((texto, caracter) => texto = caracter + texto, '')),
