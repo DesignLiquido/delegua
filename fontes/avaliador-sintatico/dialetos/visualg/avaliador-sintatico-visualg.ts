ce267081424e789318de7c4ee81a11fa9b8a2ce8
Samuel Vaz <samuel.rvg@gmail.com>
2023-09-21 19:11:32 -0300

    Melhorias limpatela

--- fontes/avaliador-sintatico/dialetos/visualg/avaliador-sintatico-visualg.ts
+++ fontes/avaliador-sintatico/dialetos/visualg/avaliador-sintatico-visualg.ts
@@ -401,34 +401,10 @@ export class AvaliadorSintaticoVisuAlg extends AvaliadorSintaticoBase {
         return declaracoes;
     }
 
-    finalizarChamada(entidadeChamada: Construto): Construto {
-        const argumentos: Array<Construto> = [];
-
-        if(this.eLimpaTela()) {
-            return new Chamada(this.hashArquivo, entidadeChamada, null, argumentos);
-        }
-
-        if (!this.verificarTipoSimboloAtual(tiposDeSimbolos.PARENTESE_DIREITO)) {
-            do {
-                if (argumentos.length >= 255) {
-                    throw this.erro(this.simbolos[this.atual], 'Não pode haver mais de 255 argumentos.');
-                }
-                argumentos.push(this.expressao());
-            } while (this.verificarSeSimboloAtualEIgualA(tiposDeSimbolos.VIRGULA));
-        }
-
-        const parenteseDireito = this.consumir(tiposDeSimbolos.PARENTESE_DIREITO, "Esperado ')' após os argumentos.");
-
-        return new Chamada(this.hashArquivo, entidadeChamada, parenteseDireito, argumentos);
-    }
-
     chamar(): Construto {
         let expressao = this.primario();
 
         while (true) {
-            if(this.eLimpaTela()) {
-                expressao = this.finalizarChamada(expressao);
-            }
             if (this.verificarSeSimboloAtualEIgualA(tiposDeSimbolos.PARENTESE_ESQUERDO)) {
                 expressao = this.finalizarChamada(expressao);
             } else if (this.verificarSeSimboloAtualEIgualA(tiposDeSimbolos.COLCHETE_ESQUERDO)) {
