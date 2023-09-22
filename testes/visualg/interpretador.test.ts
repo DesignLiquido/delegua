d23fd73b33a4c016c970aa9ab3795e26172bc11a
Samuel Vaz <samuel.rvg@gmail.com>
2023-09-22 04:49:39 -0300

    #503 Teste para matriz inválida

--- testes/visualg/interpretador.test.ts
+++ testes/visualg/interpretador.test.ts
@@ -316,6 +316,39 @@ describe('Interpretador', () => {
                 expect(retornoInterpretador.erros).toHaveLength(0);
             });
 
+            //TODO: https://github.com/DesignLiquido/delegua/issues/503
+            it.skip("Sucesso - Matriz - Jogo da Velha", async () => {
+                const retornoLexador = lexador.mapear([
+                    'Algoritmo "Jogo da Velha"',
+                    'Var',
+                        'i, q, t: inteiro',
+                        'jogoMatriz : vetor [1..3, 1..3] de caractere',
+                    'Inicio',
+                        'q <- 4',
+                       't <- 7',
+                       'para i de 1 ate 3 faca',
+                          'jogoMatriz[1,i] <- numpcarac(i)',
+                          'jogoMatriz[2,i] <- numpcarac(q)',
+                          'jogoMatriz[3,i] <- numpcarac(t)',
+                          'q <- q + 1',
+                          't <- t + 1',
+                       'fimpara',
+
+                       'Escreval("      | ", jogoMatriz[1,1], " | ", jogoMatriz[1,2], " | ", jogoMatriz[1,3], " |")',
+                       'Escreval("      +---+---+---+")',
+                       'Escreval("      | ", jogoMatriz[2,1], " | ", jogoMatriz[2,2], " | ", jogoMatriz[2,3], " |")',
+                       'Escreval("      +---+---+---+")',
+                       'Escreval("      | ", jogoMatriz[3,1], " | ", jogoMatriz[3,2], " | ", jogoMatriz[3,3], " |")',
+                    'Fimalgoritmo'
+                ], -1);
+
+                const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+                const retornoInterpretador = await interpretador.interpretar(retornoAvaliadorSintatico.declaracoes);
+
+                expect(retornoInterpretador.erros).toHaveLength(0);
+            });
+
             it('Sucesso - Para com passo negativo', async () => {
                 // Aqui vamos simular a resposta para uma variável de `leia()`.
                 const respostas = [
