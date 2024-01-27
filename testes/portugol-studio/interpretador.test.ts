46ea05d809823bceeaece7cb1e5d777b2557663e
Samuel Vaz <samuel.rvg@gmail.com>
2024-01-26 22:17:34 -0300

    Declaracao constante Portugol Studio

--- testes/portugol-studio/interpretador.test.ts
+++ testes/portugol-studio/interpretador.test.ts
@@ -271,6 +271,63 @@ describe('Interpretador (Portugol Studio)', () => {
 
                 expect(retornoInterpretador.erros).toHaveLength(0);
             });
+
+            it('Constante', async () => {
+                const respostas = ["Abigail", 4, 5, 10];
+                interpretador.interfaceEntradaSaida = {
+                    question: (mensagem: string, callback: Function) => {
+                        callback(respostas.pop());
+                    }
+                };
+
+                const retornoLexador = lexador.mapear([
+                    'programa',
+                    '{',
+                        'funcao inicio ()',
+                        '{	',                            
+
+                            'const real PRECO_PARAFUSO = 1.50',
+                            'const real PRECO_ARRUELA  = 2.00',
+                            'const real PRECO_PORCA    = 2.50',
+
+                            'cadeia nome',
+                            'inteiro quantidade_parafusos, quantidade_arruelas, quantidade_porcas',
+                            'real total_parafusos, total_arruelas, total_porcas, total_pagar',
+
+                            'escreva("Digite seu nome: ")',
+                            'leia(nome)',
+                            
+                            'escreva("\nDigite a quantidade de parafusos que deseja comprar: ")',
+                            'leia(quantidade_parafusos)',
+                            
+                            'escreva("Digite a quantidade de arruelas que deseja comprar: ")',
+                            'leia(quantidade_arruelas)',
+
+                            'escreva("Digite a quantidade de porcas que deseja comprar: ")',
+                            'leia(quantidade_porcas)',
+
+                            'total_parafusos = PRECO_PARAFUSO * quantidade_parafusos',
+                            'total_arruelas = PRECO_ARRUELA * quantidade_arruelas',
+                            'total_porcas = PRECO_PORCA * quantidade_porcas',
+                            
+                            'total_pagar = total_parafusos + total_porcas + total_arruelas',
+                            
+                            'escreva("Cliente: ", nome, "\n")',
+                            'escreva("===============================\n")',
+                            'escreva("Parafusos: ", quantidade_parafusos, "\n")',
+                            'escreva("Arruelas: " , quantidade_arruelas, "\n")',
+                            'escreva("Porcas: ", quantidade_porcas, "\n")',
+                            'escreva("===============================\n")',
+                            'escreva("Total a pagar:  R$ ", total_pagar, "\n")',
+                        '}',
+                    '}',
+                ], -1);
+                const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+
+                const retornoInterpretador = await interpretador.interpretar(retornoAvaliadorSintatico.declaracoes);
+
+                expect(retornoInterpretador.erros).toHaveLength(0);
+            });
         });
     });
 });
