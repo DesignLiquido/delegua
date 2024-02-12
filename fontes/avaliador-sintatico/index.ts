0f5725268b82dab8226c1315358b12182450db46
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-02-12 11:36:45 -0800

    Mais algumas exportações pedidas por pacotes de dialetos.

--- fontes/avaliador-sintatico/index.ts
+++ fontes/avaliador-sintatico/index.ts
@@ -1,4 +1,5 @@
 export * from './avaliador-sintatico';
+export * from './avaliador-sintatico-base';
 export * from './dialetos';
 export * from './erro-avaliador-sintatico';
 export * from './micro-avaliador-sintatico';
--- fontes/lexador/index.ts
+++ fontes/lexador/index.ts
@@ -1,3 +1,4 @@
 export * from './lexador';
+export * from './lexador-base-linha-unica';
 export * from './micro-lexador';
 export * from './simbolo';
