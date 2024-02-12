8f2c7969a6647d1d70bcdccc033779524f47ba33
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-02-11 19:36:03 -0800

    Transpilando apenas `fontes`. Outros diretórios devem ficar fora a partir de agora.

---
+++ fontes/index.ts
@@ -0,0 +1,6 @@
+export * as avaliadores from './avaliador-sintatico';
+export { PontoParada, cyrb53 } from './depuracao';
+export * as formatadores from './formatadores';
+export * as interpretadores from './interpretador';
+export * as lexadores from './lexador';
+export * as tradutores from './tradutores';
