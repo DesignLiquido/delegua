fdd7b68e16794e93192f5511dcb7e68815b09152
Leonel Sanches da Silva <leonel@designliquido.com.br>
2024-02-14 11:31:56 -0800

    Adição dos retornos às exportações do avaliador sintático.

--- fontes/avaliador-sintatico/index.ts
+++ fontes/avaliador-sintatico/index.ts
@@ -3,3 +3,4 @@ export * from './avaliador-sintatico-base';
 export * from './dialetos';
 export * from './erro-avaliador-sintatico';
 export * from './micro-avaliador-sintatico';
+export * from './retornos';
