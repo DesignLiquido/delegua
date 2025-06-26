42a8d834a7bdf87ff6b7af6ec3409e807002e89c
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2025-06-25 17:52:55 -0700

    Alguns testes unitários da própria documentação de Tenda.

--- testes/tradutores/tradutor-reverso-tenda.test.ts
+++ testes/tradutores/tradutor-reverso-tenda.test.ts
@@ -23,6 +23,35 @@ describe('Tradutor Tenda -> Delégua', () => {
             expect(resultado).toMatch(/escreva\("Oi"\)/i);
         });
 
+        it.skip('dicionário', () => {
+            const codigo = [
+                'seja dicionário = { "nome": "Tenda", "versão": 1.0 }',
+                'seja nome = dicionário["nome"]',
+                'seja versão = dicionário.versão',
+                'exiba("Nome: " + nome + ", Versão: " + versão)',
+            ];
+
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+        });
+
+        it.skip('enquanto', () => {
+            const codigo = [
+                'seja contador = 1',
+                'enquanto contador <= 5 faça',
+                '    exiba("Contador: " + contador)',
+                '    contador = contador + 1',
+                'fim'
+            ];
+
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+        });
+
         it('leia -> leia', () => {
             const codigo = `leia("Digite sua idade: ")`;
 
@@ -31,6 +60,60 @@ describe('Tradutor Tenda -> Delégua', () => {
             const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
             expect(resultado).toBeTruthy();
             expect(resultado).toMatch(/leia\(\"Digite sua idade: \"\)/i);
+        });
+
+        it.skip('operações de comparação', () => {
+            const codigo = [
+                'seja a = 10',
+                'seja b = 5',
+                'seja maior = a > b',
+                'seja menor = a < b',
+                'seja maior_ou_igual = a >= b',
+                'seja menor_ou_igual = a <= b',
+                'seja igual = a é b',
+                'seja diferente = a não é b',
+                'seja negado = não a',
+            ];
+            
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+        });
+
+        it.skip('operações de concatenação', () => {
+            const codigo = [
+                'seja texto1 = "Olá"',
+                'seja texto2 = "Mundo"',
+                'seja texto_concatenado = texto1 + " " + texto2',
+                'seja lista1 = [1, 2, 3]',
+                'seja lista2 = [4, 5, 6]',
+                'seja lista_concatenada = lista1 + lista2'
+            ];
+            
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
+        });
+
+        it.skip('operações matemáticas', () => {
+            const codigo = [
+                "seja a = 10",
+                "seja b = 5",
+                "seja soma = a + b",
+                "seja subtração = a - b",
+                "seja multiplicação = a * b",
+                "seja divisão = a / b",
+                "seja resto = a % b",
+                "seja potência = a ^ b",
+                "seja negativo = -a",
+            ];
+            
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
         });
 
         it('seja -> var', () => {
@@ -46,6 +129,20 @@ describe('Tradutor Tenda -> Delégua', () => {
             expect(resultado).toMatch(/var nome = \"Tenda\"/i);
             expect(resultado).toMatch(/var idade = 10/i);
             expect(resultado).toMatch(/var lista = \[1, 2, 3, 4, 5\]/i);
+        });
+
+        it.skip('para cada', () => {
+            const codigo = [
+                "seja total = 0",
+                "para cada i em 1 até 10 faça",
+                "    total = total + i",
+                "fim",
+                "exiba(total)",
+            ];
+            const retornoLexador = lexador.mapear(codigo, -1);
+            const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+            const resultado = tradutor.traduzir(retornoAvaliadorSintatico.declaracoes);
+            expect(resultado).toBeTruthy();
         });
 
         it('se senão', () => {
