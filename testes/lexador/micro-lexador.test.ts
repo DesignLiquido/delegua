8c89fb11cd9b7da6be159fe59d5040af5c70dc0d
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2025-09-01 17:39:43 -0700

    Suporte a vetores literais e acesso a variáveis por índice na micro-lexação e micro-avaliação sintática. (#841)

--- testes/lexador/micro-lexador.test.ts
+++ testes/lexador/micro-lexador.test.ts
@@ -9,46 +9,53 @@ describe('Lexador', () => {
         });
 
         describe('Cenários de sucesso', () => {
-            it('Sucesso - Código vazio', () => {
+            it('Código vazio', () => {
                 const resultado = microLexador.mapear('');
 
                 expect(resultado).toBeTruthy();
                 expect(resultado.simbolos).toHaveLength(0);
             });
 
-            it('Sucesso - Literal', () => {
+            it('Literal', () => {
                 const resultado = microLexador.mapear('2');
 
                 expect(resultado).toBeTruthy();
                 expect(resultado.simbolos).toHaveLength(1);
             });
 
-            it('Sucesso - Variável', () => {
+            it('Variável', () => {
                 const resultado = microLexador.mapear('teste');
 
                 expect(resultado).toBeTruthy();
                 expect(resultado.simbolos).toHaveLength(1);
             });
 
-            it('Sucesso - Soma', () => {
+            it('Soma', () => {
                 const resultado = microLexador.mapear('teste + 2');
 
                 expect(resultado).toBeTruthy();
                 expect(resultado.simbolos).toHaveLength(3);
             });
 
-            it('Sucesso - Operações matemáticas encadeadas', () => {
+            it('Operações matemáticas encadeadas', () => {
                 const resultado = microLexador.mapear('1 * 2 - 3 % 4');
 
                 expect(resultado).toBeTruthy();
                 expect(resultado.simbolos).toHaveLength(7);
             });
 
-            it('Sucesso - Operações matemáticas encadeadas', () => {
+            it('Operações matemáticas encadeadas', () => {
                 const resultado = microLexador.mapear('somar(2, 3)');
 
                 expect(resultado).toBeTruthy();
                 expect(resultado.simbolos).toHaveLength(6);
+            });
+
+            it('Vetores literais', () => {
+                const resultado = microLexador.mapear('[1, 2, 3, 4, 5]');
+
+                expect(resultado).toBeTruthy();
+                expect(resultado.simbolos).toHaveLength(11);
             });
         });
     });
