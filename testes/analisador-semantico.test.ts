f230e70aeb059d497e14abcd4fa0f79fccae59bc
João Lucas Carvalho <88744662+Joaolpridolficarvalho@users.noreply.github.com>
2025-10-07 15:46:28 -0300

    Adiciona testes para o comando 'quebrar' (#898)
    * Adiciona testes para o comando 'quebrar'
    * Corrige teste de falha

--- testes/analisador-semantico.test.ts
+++ testes/analisador-semantico.test.ts
@@ -716,4 +716,57 @@ describe('Analisador semântico', () => {
             });
         });
     });
-});
+    describe('Comando quebrar', () => {
+        describe('Cenários com laço for', () => {
+            it('Sucesso - dentro do laço for', () => {
+                const retornoLexador = lexador.mapear([
+                    "para (var i = 0; i < 10; i++) {",
+                        "   se (i == 5) {",
+                        "       quebrar;",
+                        "   }",
+                ], -1)
+                const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+                const retornoAnalisadorSemantico = analisadorSemantico.analisar(retornoAvaliadorSintatico.declaracoes);
+                expect(retornoAnalisadorSemantico).toBeTruthy();
+                expect(retornoAnalisadorSemantico.diagnosticos).toHaveLength(0);
+            });
+            it('Sucesso - dentro do laço enquanto', () => {
+                const retornoLexador = lexador.mapear([
+                    "enquanto (verdadeiro) {",
+                        "   se (i == 5) {",
+                        "       quebrar;",
+                        "   }",
+                ], -1)
+                const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+                const retornoAnalisadorSemantico = analisadorSemantico.analisar(retornoAvaliadorSintatico.declaracoes);
+                expect(retornoAnalisadorSemantico).toBeTruthy();
+                expect(retornoAnalisadorSemantico.diagnosticos).toHaveLength(0);
+            });
+            it('Sucesso - dentro do laço faça...enquanto', () => {
+                const retornoLexador = lexador.mapear([
+                    "faça {",
+                        "   se (i == 5) {",
+                        "       quebrar;",
+                        "   }",
+                    "} enquanto (verdadeiro);",
+                ], -1)
+                const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+                const retornoAnalisadorSemantico = analisadorSemantico.analisar(retornoAvaliadorSintatico.declaracoes);
+                expect(retornoAnalisadorSemantico).toBeTruthy();
+                expect(retornoAnalisadorSemantico.diagnosticos).toHaveLength(0);
+            });
+            it('Erro - fora de laço', () => {
+                const retornoLexador = lexador.mapear([
+                    "quebrar;",
+                ], -1)
+                const retornoAvaliadorSintatico = avaliadorSintatico.analisar(retornoLexador, -1);
+                const retornoAnalisadorSemantico = analisadorSemantico.analisar(retornoAvaliadorSintatico.declaracoes)
+                expect(retornoAnalisadorSemantico).toBeTruthy();
+                expect(retornoAnalisadorSemantico.diagnosticos).toHaveLength(1);
+                expect(retornoAnalisadorSemantico.diagnosticos.filter(item => item.severidade === DiagnosticoSeveridade.ERRO)).toHaveLength(1);
+              });
+        }
+        );   
+    });
+}
+)
