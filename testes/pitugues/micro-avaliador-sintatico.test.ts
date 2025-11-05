461ae0a323ffe8052b61b4d18c52a0ac6efb115c
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2025-11-05 13:58:18 -0800

    `imprima` não faz sentido na micro avaliação sintática.

--- testes/pitugues/micro-avaliador-sintatico.test.ts
+++ testes/pitugues/micro-avaliador-sintatico.test.ts
@@ -1,5 +1,6 @@
 
 import  {MicroAvaliadorSintaticoPitugues}  from '../../fontes/avaliador-sintatico/dialetos/micro-avaliador-sintatico-pitugues';
+import { Binario } from '../../fontes/construtos';
 import { LexadorPitugues } from "../../fontes/lexador/dialetos";
 
 describe('MicroAvaliadorSintatico (Pituguês)', () => {
@@ -13,17 +14,18 @@ describe('MicroAvaliadorSintatico (Pituguês)', () => {
             microAvaliadorSintatico = new MicroAvaliadorSintaticoPitugues();
         });
         describe('Casos de sucesso', () => {
-            it('Olá Mundo', () => {
+            it('Operações matemáticas básicas', () => {
                 const retornoLexador = lexador.mapear(
-                    ["var nome = 'Design Liquido'",
-                        "imprima(Olá, {nome}!)"
+                    [
+                        "3 + 2 * 5 / 8"
                     ],
                     HASH_ARQUIVO_EXEMPLO
                 );
                 const retornoMicroAvaliadorSintatico =
                     microAvaliadorSintatico.analisar(retornoLexador, HASH_ARQUIVO_EXEMPLO);
                 expect(retornoMicroAvaliadorSintatico).toBeTruthy();
-                expect(retornoMicroAvaliadorSintatico.declaracoes).toHaveLength(2);
+                expect(retornoMicroAvaliadorSintatico.declaracoes).toHaveLength(1);
+                expect(retornoMicroAvaliadorSintatico.declaracoes[0].constructor).toBe(Binario);
             });
             
         });
