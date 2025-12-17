fc2bd508e66f81c25b14f5189a7ae35232a6b7cd
Victor Francelino Sena Santos <88949817+VictorFrancelino@users.noreply.github.com>
2025-12-17 14:27:54 -0400

    Troca 'tupla-pitugues' por 'tupla-n'

--- fontes/construtos/index.ts
+++ fontes/construtos/index.ts
@@ -42,7 +42,7 @@ export * from './super';
 export * from './tipo-de';
 export * from './tupla';
 export * from './tuplas';
-export * from './tupla-pitugues';
+export * from './tupla-n';
 export * from './unario';
 export * from './variavel';
 export * from './vetor';
