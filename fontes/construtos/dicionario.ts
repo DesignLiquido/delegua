fc8d579e650bd78ffabdae77ab7f79fa7082527c
Aristides da Costa <aristidiscosta200@gmail.com>
2025-12-26 20:47:09 +0100

    (Des)empacotamento  para dicionario (#1040)
    * feat: added syntactic evaluator for (Un)packaging with **
    * test: coverage tests for the syntactic evaluator of (Un)packing with **
    * feat: Interpreting (Un)packaging with **
    * test: coverage tests for the (Un)packaging interpreter with **
    * feat: Adding translators for (Un)packaging with **
    * refactor: Removed the implementation of the spread in the classic mare
    * refactor: The spread implementation has been removed from the interpreter.
    * refactor: Modifications from the Tenda translator have been removed.

--- fontes/construtos/dicionario.ts
+++ fontes/construtos/dicionario.ts
@@ -8,14 +8,16 @@ export class Dicionario implements Construto {
     chaves: any[];
     valores: Construto[];
     tipo: 'dicionário';
+    esSpread: boolean[];
 
-    constructor(hashArquivo: number, linha: number, chaves: any[], valores: Construto[]) {
+    constructor(hashArquivo: number, linha: number, chaves: any[], valores: Construto[], esSpread?: boolean[]) {
         this.linha = linha;
         this.hashArquivo = hashArquivo;
 
         this.chaves = chaves;
         this.valores = valores;
         this.tipo = 'dicionário';
+        this.esSpread = esSpread || chaves.map(() => false);
     }
 
     async aceitar(visitante: VisitanteComumInterface): Promise<any> {
