89ff0ce4ea372854793c4fedfc9a1f7cb1cbf30e
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2026-02-05 15:00:24 -0800

    Casos adicionais de inferência de tipos encontrados em `delegua-node`.

--- fontes/interpretador/estruturas/classe-de-modulo.ts
+++ fontes/interpretador/estruturas/classe-de-modulo.ts
@@ -1,5 +1,14 @@
 import { Chamavel } from './chamavel';
 
+export type MetodoDeClasseDeModulo = {
+    tipoRetorno: string;
+    argumentos: { nome: string; tipo: string }[];
+}
+
+export type PropriedadeDeClasseDeModulo = {
+    tipo: string;
+}
+
 /**
  * Uma classe de módulo não é muito diferente de uma `ClassePadrao`, com o adicional
  * de ter documentações extras para métodos e propriedades.
@@ -8,8 +17,8 @@ export class ClasseDeModulo extends Chamavel {
     nome: string;
     modulo: string;
     implementacao: any;
-    metodos: { [nome: string]: any };
-    propriedades: { [nome: string]: any };
+    metodos: { [nome: string]: MetodoDeClasseDeModulo };
+    propriedades: { [nome: string]: PropriedadeDeClasseDeModulo };
 
     constructor(
         nome: string,
