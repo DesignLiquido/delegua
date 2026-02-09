1784da815ff8a0e5e3e757608872d593a446df53
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2026-02-09 10:36:08 -0800

    Ajustando interpretador de Égua Clássico para regras de métodos contidas na implementação original.

--- fontes/interpretador/dialetos/egua-classico/interpretador-egua-classico.ts
+++ fontes/interpretador/dialetos/egua-classico/interpretador-egua-classico.ts
@@ -487,8 +487,8 @@ export class InterpretadorEguaClassico implements InterpretadorInterface {
         if (entidadeChamada instanceof DeleguaFuncao) {
             parametros = entidadeChamada.declaracao.parametros;
         } else if (entidadeChamada instanceof DescritorTipoClasse) {
-            const metodoInit = entidadeChamada.metodos.inicializacao;
-            parametros = metodoInit && !Array.isArray(metodoInit)
+            const metodoInit = entidadeChamada.metodos.inicializacao as DeleguaFuncao;
+            parametros = metodoInit
                 ? metodoInit.declaracao.parametros
                 : [];
         } else {
@@ -965,7 +965,7 @@ export class InterpretadorEguaClassico implements InterpretadorInterface {
             this.pilhaEscoposExecucao.definirVariavel('super', superClasse);
         }
 
-        const metodos: { [nome: string]: DeleguaFuncao | DeleguaFuncao[] } = {};
+        const metodos: { [nome: string]: DeleguaFuncao } = {};
         const definirMetodos = declaracao.metodos;
         for (let i = 0; i < declaracao.metodos.length; i++) {
             const metodoAtual = definirMetodos[i];
@@ -976,15 +976,7 @@ export class InterpretadorEguaClassico implements InterpretadorInterface {
                 undefined,
                 eInicializador
             );
-            const nomeMetodo = metodoAtual.simbolo.lexema;
-            if (metodos[nomeMetodo]) {
-                if (!Array.isArray(metodos[nomeMetodo])) {
-                    metodos[nomeMetodo] = [metodos[nomeMetodo] as DeleguaFuncao];
-                }
-                (metodos[nomeMetodo] as DeleguaFuncao[]).push(funcao);
-            } else {
-                metodos[nomeMetodo] = funcao;
-            }
+            metodos[metodoAtual.simbolo.lexema] = funcao;
         }
 
         const deleguaClasse = new DescritorTipoClasse(declaracao.simbolo, superClasse, metodos);
