dd7352cc596762485f56ba8a469a53370a5875d3
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2026-02-17 16:44:34 -0800

    Implementando comando `'pausar'` na depuração, usado por IDEs.

--- fontes/interpretador/depuracao/interpretador-base-com-depuracao.ts
+++ fontes/interpretador/depuracao/interpretador-base-com-depuracao.ts
@@ -1,5 +1,5 @@
 import { Binario, Chamada, Construto } from '../../construtos';
-import { Declaracao, Enquanto, Escreva, Para, Retorna } from '../../declaracoes';
+import { Declaracao, Enquanto, Escreva, Fazer, Para, Retorna } from '../../declaracoes';
 import { PontoParada } from '../../depuracao';
 import {
     ComandoDepurador,
@@ -146,6 +146,10 @@ export class InterpretadorBaseComDepuracao
 
     override async visitarDeclaracaoPara(declaracao: Para): Promise<any> {
         return await comum.visitarDeclaracaoPara(this, declaracao);
+    }
+
+    override async visitarDeclaracaoFazer(declaracao: Fazer): Promise<any> {
+        return await comum.visitarDeclaracaoFazer(this, declaracao);
     }
 
     /**
