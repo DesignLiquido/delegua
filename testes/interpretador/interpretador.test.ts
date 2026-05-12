2bbfc3c9a6bd0f3f6abd8be7f466a6deace103e2
VictorFrancelino <victor.francelino10@gmail.com>
2026-05-12 00:29:27 -0400

    fix: corrige estrutura 'escolha' para interromper no primeiro 'caso' que satisfaz

--- testes/interpretador/interpretador.test.ts
+++ testes/interpretador/interpretador.test.ts
@@ -1534,6 +1534,28 @@ describe('Interpretador', () => {
 
                     expect(retornoInterpretador.erros).toHaveLength(0);
                 });
+
+                it('Deve parar o loop quando encontrar o primeiro "caso" que satisfaz', async () => {
+                    const retornoLexador = lexador.mapear([
+                        'var dia = "segunda"',
+                        'escolha (dia) {',
+                        '   caso "segunda":',
+                        '       escreva("Início da semana")',
+                        '   caso "segunda":',
+                        '       escreva("Isso era pra ser executado de novo?")',
+                        '   caso "sexta":',
+                        '       escreva("Final de semana")',
+                        '}'
+                    ], -1);
+                    const retornoAvaliadorSintatico = await avaliadorSintatico
+                        .analisar(retornoLexador, -1);
+                    const retornoInterpretador = await interpretador
+                        .interpretar(retornoAvaliadorSintatico.declaracoes);
+
+                    expect(retornoInterpretador.erros).toHaveLength(0);
+                    expect(_saidas.length).toBe(1);
+                    expect(_saidas[0]).toBe('Início da semana');
+                });
             });
 
             describe('escreva()', () => {
