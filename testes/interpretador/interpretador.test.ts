8b80e030a167a8b709f9f3b371e1134db7048816
Victor Francelino <88949817+VictorFrancelino@users.noreply.github.com>
2026-05-22 12:55:44 -0400

    fix: `retorna` respeita valores iniciados com operador unário (#1291)

--- testes/interpretador/interpretador.test.ts
+++ testes/interpretador/interpretador.test.ts
@@ -4759,6 +4759,23 @@ describe('Interpretador', () => {
                 expect(_saidas.length).toBe(1);
                 expect(_saidas[0]).toBe("número");
             });
+
+            it('Deve respeitar valores iniciados por operador unário quando usado "retorna"', async () => {
+                const retornoLexador = lexador.mapear([
+                    'funcao negativo() {',
+                    '    retorna -42 ',
+                    '}',
+                    'escreva(negativo())'
+                ], -1);
+                const retornoAvaliadorSintatico = await avaliadorSintatico
+                    .analisar(retornoLexador, -1);
+                const retornoInterpretador = await interpretador
+                    .interpretar(retornoAvaliadorSintatico.declaracoes);
+
+                expect(retornoInterpretador.erros).toHaveLength(0);
+                expect(_saidas.length).toBe(1);
+                expect(_saidas[0]).toBe("-42");
+            });
         });
 
         describe('Ajuda', () => {
