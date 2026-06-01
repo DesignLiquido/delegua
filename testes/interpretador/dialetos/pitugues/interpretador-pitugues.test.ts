d6bd55d3f071189289a54e76948c701ddaef9df6
Victor Francelino <88949817+VictorFrancelino@users.noreply.github.com>
2026-06-01 17:21:00 -0400

    test: adiciona testes de inferência de tipo `lógico` em retorno de função com comparação (#1323)

--- testes/interpretador/dialetos/pitugues/interpretador-pitugues.test.ts
+++ testes/interpretador/dialetos/pitugues/interpretador-pitugues.test.ts
@@ -4513,6 +4513,23 @@ describe('Interpretador (Pituguês)', () => {
                 expect(_saidas).toHaveLength(1);
                 expect(_saidas[0]).toBe('-9');
             });
+
+            it('Deve inferir tipo lógico no retorno de função com comparação', async () => {
+                const codigo = [
+                    'funcao ola(nome):',
+                    '    escreva(f"Olá, eu me chamo {nome}")',
+                    '    retorna nome != 10',
+                    'escreva(tipo(ola("Victor")))'
+                ];
+                const retornoLexador = lexador.mapear(codigo, -1);
+                const retornoAvaliadorSintatico = await avaliadorSintatico.analisar(retornoLexador, -1);
+                const retornoInterpretador = await interpretador.interpretar(retornoAvaliadorSintatico.declaracoes);
+
+                expect(retornoInterpretador.erros).toHaveLength(0);
+                expect(_saidas).toHaveLength(2);
+                expect(_saidas[0]).toBe('Olá, eu me chamo Victor');
+                expect(_saidas[1]).toBe('lógico');
+            });
         });
 
         it('termina_com - sufixo encontrado no final', async () => {
--- testes/interpretador/interpretador.test.ts
+++ testes/interpretador/interpretador.test.ts
@@ -4873,6 +4873,24 @@ describe('Interpretador', () => {
                 expect(_saidas).toHaveLength(1);
                 expect(_saidas[0]).toBe('-9');
             });
+
+            it('Deve inferir tipo lógico no retorno de função com comparação', async () => {
+                const codigo = [
+                    'funcao ola(nome) {',
+                    '    escreva("Olá, eu me chamo ${nome}")',
+                    '    retorna nome != 10',
+                    '}',
+                    'escreva(tipo de ola("Victor"))'
+                ];
+                const retornoLexador = lexador.mapear(codigo, -1);
+                const retornoAvaliadorSintatico = await avaliadorSintatico.analisar(retornoLexador, -1);
+                const retornoInterpretador = await interpretador.interpretar(retornoAvaliadorSintatico.declaracoes);
+
+                expect(retornoInterpretador.erros).toHaveLength(0);
+                expect(_saidas).toHaveLength(2);
+                expect(_saidas[0]).toBe('Olá, eu me chamo Victor');
+                expect(_saidas[1]).toBe('lógico');
+            });
         });
 
         describe('Ajuda', () => {
