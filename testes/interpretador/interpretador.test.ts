457cc5209e904258fc1dda2c6bca456afd66618c
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2026-08-19 10:11:36 -0700

    Teste unitário para `escreva(tipo de 3.141592)`.

--- testes/interpretador/interpretador.test.ts
+++ testes/interpretador/interpretador.test.ts
@@ -7530,6 +7530,15 @@ describe('Interpretador', () => {
                 expect(_saidas[0]).toContain('número');
             });
 
+            it('tipo de literal número real', async () => {
+                const codigo = ['escreva(tipo de 3.141592)'];
+                const retornoLexador = lexador.mapear(codigo, -1);
+                const retornoAvaliadorSintatico = await avaliadorSintatico.analisar(retornoLexador, -1);
+                const retornoInterpretador = await interpretador.interpretar(retornoAvaliadorSintatico.declaracoes);
+                expect(retornoInterpretador.erros).toHaveLength(0);
+                expect(_saidas[0]).toContain('real');
+            });
+
             it('tipo de literal texto', async () => {
                 const codigo = ['escreva(tipo de "teste")'];
                 const retornoLexador = lexador.mapear(codigo, -1);
